e729931e2de6cdb39ba635d03539a4f5c1e0e80a
TrieuTienThanh <trieutienthanhmmd@gmail.com>
2025-11-29 15:50:24 +0700

    Update

--- interface.py
+++ interface.py
@@ -3,4 +3,7 @@ root = Tk()
 root.title("Quản lý siêu thị")
 root.minsize(height = 600, width = 1000)
 
+Label(root, text = 'Quản lý siêu thị', fg = 'red', font = ('Cambria', 20), width = 63).grid(row = 0)
+
+
 root.mainloop()
